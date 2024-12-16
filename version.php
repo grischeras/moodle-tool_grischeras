b69d5712e2cc80cf89b55a123a28f9f7f0dc89c3
Alberto <alberto.sempreboni@yocabe.com>
2024-12-16 16:29:38 +0000

    initial commit

---
+++ version.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version metadata for the plugintype_pluginname plugin.
+ *
+ * @package   tool_grischeras
+ * @copyright 2024, Alberto Sempreboni <alberto.sempreboni@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2024121600;
+$plugin->requires =2023103000;
+$plugin->supported = [400,500];
+//$plugin->incompatible = 400;
+$plugin->component = 'tool_grischeras';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';
+
+// $plugin->dependencies = [
+//     'mod_forum' => 2022042100,
+//     'mod_data' => 2022042100
+// ];
