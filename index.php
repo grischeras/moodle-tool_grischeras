79888f0f1ddebdbc2acf410a8ed8ef336f896e53
Alberto <alberto.sempreboni@yocabe.com>
2024-12-18 10:15:29 +0000

    WIP admin_externalpage_setup doesn't work

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <?php
 
 require_once(__DIR__ . '/../../../config.php');
+require_once($CFG->libdir.'/adminlib.php');
+
+
+admin_externalpage_setup('toolgrischeras');
+// Set up the page.
+$title = get_string('pluginname', 'tool_grischeras');
+$pagetitle = $title;
+
 $url = new moodle_url('/admin/tool/grischeras/index.php');
+
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url($url);
-$PAGE->set_pagelayout('report');
-$PAGE->set_title('Hello to My Plugin!');
-$PAGE->set_heading(get_string('pluginname', 'tool_grischeras'));
+$PAGE->set_title($title);
+$PAGE->set_heading($title);
 
-echo  get_string('pluginname', 'tool_grischeras');
+$output = $PAGE->get_renderer('tool_grischeras');
+
+
+echo $output->header();
+echo $output->heading($pagetitle);
+
+$renderable = new \tool_demo\output\index_page('Some text');
+echo $output->render($renderable);
+
+echo $output->footer();
