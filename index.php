521a941b5ef08f2a59671d51b7a99b0e7fec3731
Alberto <alberto.sempreboni@yocabe.com>
2024-12-18 16:18:30 +0000

    a working template rendering + breadcumbs

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
 
 require_once(__DIR__ . '/../../../config.php');
-//require_once($CFG->libdir.'/adminlib.php');
 
 require_login(null, false);
+
 // Set up the page.
 $title = get_string('pluginname', 'tool_grischeras');
 $pagetitle = $title;
 
 $url = new moodle_url('/admin/tool/grischeras/index.php');
+
+/** BREADCUMBS*/
+$previewnode = $PAGE->navigation->add(
+    get_string('home'),
+    new moodle_url('/index.php'),
+    navigation_node::TYPE_CONTAINER
+);
+$thingnode = $previewnode->add(
+    $title,
+   $url
+);
+$thingnode->make_active();
+/** BREADCUMBS END */
 
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url($url);
