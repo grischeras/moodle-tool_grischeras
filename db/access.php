1320d94e29743fe02e761a448de0ffc223e54d18
Alberto <alberto.sempreboni@yocabe.com>
2024-12-23 16:30:21 +0000

    Added a query to retrieve data > code style fix

--- db/access.php
+++ db/access.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -17,6 +16,7 @@
 
 /**
  * defining access rules
+ *
  * @package   tool_grischeras
  * @copyright 2024, Alberto Sempreboni <alberto.sempreboni@moodle.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
--- lib.php
+++ lib.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * this file adds a new menu voice for the course settings menu
+ *
  * @package   tool_grischeras
  * @copyright 2024, Alberto Sempreboni <alberto.sempreboni@moodle.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
