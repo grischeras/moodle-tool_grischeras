b1a564a31c9bc4d14c616f27ccf67211108f4037
grischer <grischer.as@gmail.com>
2024-12-27 11:15:14 +0000

    added DDL code and function

---
+++ db/upgrade.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * a short description about upgrade.php]
+ * @package    tool_grischeras
+ * @copyright  2024 Alberto Sempreboni
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+/**
+ * adjusting needed databsase if the version is < than the expected
+ *
+ * @param $oldversion
+ * @return bool
+ */
+function xmldb_tool_grischeras_upgrade($oldversion): bool {
+    global $DB;
+
+    // Load the DDL manager and xmldb API.
+    $dbman = $DB->get_manager();
+    if ($oldversion < '2024122700') {
+        $table = new xmldb_table('tool_grischeras');
+
+        // Adding fields to table.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('name', XMLDB_TYPE_CHAR, '255', null, null, null, null);
+        $table->add_field('completed', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('priority', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '1');
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+
+        // Adding keys to table.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('courseid', XMLDB_KEY_FOREIGN, ['courseid'], 'course', ['id']);
+
+        // Adding indexes to table.
+        $table->add_index('courseidname', XMLDB_INDEX_UNIQUE, ['name', 'courseid']);
+
+        // Conditionally launch create table.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // savepoint reached.
+        upgrade_plugin_savepoint(true, 2024122700, 'tool', 'grischeras');
+    }
+
+    return true;
+}
+
